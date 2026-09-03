4ffc1c06e346a7f62d0467dfef639a648770e196
Ayush Solanki <aayushdhatri@gmail.com>
2026-09-03 15:16:01 +0530

    Added dto

---
+++ src/main/java/com/example/StockExchangeLLD/OrderRequest.java
@@ -0,0 +1,23 @@
+package com.example.StockExchangeLLD;
+
+import com.example.StockExchangeLLD.models.OrderType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+    private String userId;
+
+    private OrderType orderType;
+
+    private String stockId;
+
+    private int quantity;
+
+    private double price;
+}
